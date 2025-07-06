44a7589d349844bf44a008537bc066b9792d796e
TejaswiniBonam <130749202+TejaswiniBonam@users.noreply.github.com>
2025-07-06 18:09:54 +0530

    update

--- new-app/src/project/Home.jsx
+++ new-app/src/project/Home.jsx
@@ -1,24 +1,72 @@
+import {useState} from 'react';
+import axios from 'axios';
+
 export default function Home(){
-    const info = fetchData();
+    getUsers();
     return(
         <>
-        <h1> Welcome Home </h1>
-        {
-            info.forEach(p=>{
-                return <p>{p.user}</p>;
-            })
-        }
+        <Header />
+        <NaviBar />
+        <Footer />
         </>
     );
 }
-async function fetchData(){
-    const url = 'https://jsonplaceholder.typicode.com/posts';
-    try{
-        const data = await fetch(url);
-        const posts = await data.json();
-        return posts;
-    }catch(error){
-        console.log(error);
-    }
-    
+
+
+function NaviBar(){
+    return(
+        <div>
+            <nav>
+                <li><a>Home</a></li>
+                <li><a>Posts</a></li>
+                <li><a>About Us</a></li>
+            </nav>
+        </div>
+    );
 }
+
+
+function Header(){
+    return(
+    <div>
+        <h1>JSONPlaceholder Viewer</h1>
+    </div>
+    );
+}
+
+function Footer(){
+    return(
+        <div>
+            <p>This is footer</p>
+        </div>
+    );
+}
+
+function Posts(){
+    const [posts, setPosts] = useState([]);
+
+}
+
+async function getPosts(){
+    try{
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+        const postList = response.data;
+        //console.log(userList.data);
+    }
+    catch(Error){
+        alert(Error);
+    }
+
+}
+async function getUsers(){
+    try{
+        const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+        //console.log(userList.data);
+        const userList = response.data;
+    }
+    catch(Error){
+        alert(Error);
+    }
+
+}
+
